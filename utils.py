b3b279157bfd8101b2538d3cd77b5ca49607bf1d
Amir Mohamad Maniei <110853819+Amirmohamadmaniei@users.noreply.github.com>
2023-02-15 14:03:05 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,5 +8,5 @@ def create_send_otp(phone):
     code = random.randint(1000, 9999)
     OTP.objects.create(code=code, phone=phone)
 
-    data = {'from': '50004001496657', 'to': phone, 'text': f' تاپ کالا \n کد تایید : {code}'}
-    requests.post('https://console.melipayamak.com/api/send/simple/1fa73755229242dfa65bb56f70960f2d', json=data)
+    data = {'from': '', 'to': phone, 'text': f' تاپ کالا \n کد تایید : {code}'}
+    requests.post('https://console.melipayamak.com/api/', json=data)
